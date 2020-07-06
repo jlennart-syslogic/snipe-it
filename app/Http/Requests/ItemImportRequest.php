d2266bc2f7dd90046632b43b01e7a63059ff1aa7
Joe Lennart <jlennart@syslogicinc.com>
2020-07-06 11:51:58 -0500

    Update Memory Limit

--- app/Http/Requests/ItemImportRequest.php
+++ app/Http/Requests/ItemImportRequest.php
@@ -33,7 +33,7 @@ class ItemImportRequest extends FormRequest
     public function import(Import $import)
     {
         ini_set('max_execution_time', 600); //600 seconds = 10 minutes
-        ini_set('memory_limit', '500M');
+        ini_set('memory_limit', '1000M');
         $filename = config('app.private_uploads') . '/imports/' . $import->file_path;
         $import->import_type = $this->input('import-type');
         $class = title_case($import->import_type);
